0b9474b69b0bbd2ed41180cf1af668ee9a390f77
Aashish Khadka <aashishkhadka720@gmail.com>
2025-05-29 23:42:26 +0545

    refactor: simplify CORS configuration by reverting to a single allowed origin

--- index.js
+++ index.js
@@ -9,27 +9,10 @@ import Pdf from "./Routes/Pdf.Route.js";
 const app = express();
 
 // Configure CORS to allow requests from your frontend domain
-
-app.use(
-    cors({
-        origin: (origin, callback) => {
-            const allowedOrigins = [
-                "https://note-frontend-fawn.vercel.app",
-                "http://localhost:5173"
-            ];
-            if (!origin || allowedOrigins.includes(origin)) {
-                callback(null, true);
-            } else {
-                callback(new Error("Not allowed by CORS"));
-            }
-        },
-        credentials: true,
-        methods: ["GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"],
-        allowedHeaders: ["Content-Type", "Authorization", "Set-Cookie"],
-        exposedHeaders: ["Set-Cookie"]
-    })
-);
-
+app.use(cors({
+  origin: "https://note-frontend-fawn.vercel.app",
+  credentials: true
+}));
 
 app.use(express.json());
 
